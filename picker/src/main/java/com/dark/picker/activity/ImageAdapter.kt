cfec942d29521f8427e61b24403f42b2b9c756e8
Dungnm <nguyenminhdung321998@gmail.com>
2021-07-14 23:36:51 +0700

    select image android take photo

--- picker/src/main/java/com/dark/picker/activity/ImageAdapter.kt
+++ picker/src/main/java/com/dark/picker/activity/ImageAdapter.kt
@@ -1,16 +1,30 @@
 package com.dark.picker.activity
 
 import android.net.Uri
-import android.view.LayoutInflater
 import android.view.View
 import com.bumptech.glide.Glide
 import com.dark.picker.R
 import com.dark.picker.loadmore.adapter.BaseRclvVH
 import com.dark.picker.loadmore.adapter.LoadMoreAdapter
 import com.dark.picker.model.MediaGallery
+import com.dark.picker.utils.setSafeOnClickListener
 import kotlinx.android.synthetic.main.item_image.view.*
 
 class ImageAdapter : LoadMoreAdapter() {
+
+    companion object {
+        const val PAYLOAD_SELECTED = 1
+        const val MAX_ITEM_SELECTED = 30
+    }
+
+    var onSelectItem: (Boolean) -> Unit = {
+
+    }
+
+    private var maxSizeSelected: Int = MAX_ITEM_SELECTED
+
+    private val listSelected = mutableListOf<MediaGalleryData>()
+
     override fun getLoadingLayoutRes(): Int {
         return R.layout.item_load_more
     }
@@ -23,18 +37,107 @@ class ImageAdapter : LoadMoreAdapter() {
         return ImageVH(itemView)
     }
 
-    inner class ImageVH(itemView: View) : BaseRclvVH<MediaGallery>(itemView) {
-        override fun onBind(data: MediaGallery) {
+    override fun reset(newItems: List<*>?) {
+        listSelected.clear()
+        val list = newItems?.map {
+            MediaGalleryData(it as MediaGallery)
+        }
+        super.reset(list)
+    }
+
+    override fun addMoreItem(itemList: List<*>, stillMore: Boolean) {
+        val list = itemList?.map {
+            MediaGalleryData(it as MediaGallery)
+        }
+        super.addMoreItem(list, stillMore)
+    }
+
+    fun setMaxSizeSelected(max: Int) {
+        maxSizeSelected = max
+        if (listSelected.size > maxSizeSelected) {
+            listSelected.subList(0, maxSizeSelected - 1)
+        }
+        notifyDataSetChanged()
+    }
+
+    fun getListUriImageSelected(): List<Uri> {
+        return listSelected.map {
+            Uri.parse(it.data.path)
+        }
+    }
+
+    private fun getIndexSelected(item: MediaGalleryData): Int {
+        return listSelected.indexOfFirst {
+            it.data.id == item.data.id
+        } + 1
+    }
+
+    private fun findIndexOfItem(item: MediaGalleryData): Int {
+        return mDataSet.indexOfFirst {
+            item.data.id == (it as? MediaGalleryData)?.data?.id
+        }
+    }
+
+    inner class ImageVH(itemView: View) : BaseRclvVH<MediaGalleryData>(itemView) {
+
+        init {
+            itemView.setSafeOnClickListener {
+                val item = mDataSet[bindingAdapterPosition] as? MediaGalleryData
+                    ?: return@setSafeOnClickListener
+                if (item.isSelected) {
+                    val indexRemove = listSelected.indexOfFirst {
+                        item.data.id == it.data.id
+                    }
+                    if (indexRemove <= -1) {
+                        return@setSafeOnClickListener
+                    }
+                    item.isSelected = false
+                    val listSub = listSelected.slice(indexRemove until listSelected.size)
+                    listSelected.removeAt(indexRemove)
+                    notifyItemChanged(bindingAdapterPosition, PAYLOAD_SELECTED)
+                    listSub.forEach {
+                        notifyItemChanged(findIndexOfItem(it), PAYLOAD_SELECTED)
+                    }
+                } else {
+                    item.isSelected = true
+                    listSelected.add(item)
+                    notifyItemChanged(bindingAdapterPosition, PAYLOAD_SELECTED)
+                }
+                onSelectItem(listSelected.isNotEmpty())
+            }
+        }
+
+        override fun onBind(data: MediaGalleryData) {
+            selectedItem(data)
             Glide.with(itemView.ivImageItem)
-                .load(data.path)
+                .load(data.data.path)
                 .centerCrop()
                 .placeholder(R.drawable.shape_blue)
                 .into(itemView.ivImageItem)
         }
 
-        override fun onBind(data: MediaGallery, payload: List<Any>) {
-
+        override fun onBind(data: MediaGalleryData, payload: List<Any>) {
+            payload.forEach {
+                if (it is Int && it == PAYLOAD_SELECTED) {
+                    selectedItem(data)
+                }
+            }
         }
 
+        private fun selectedItem(data: MediaGalleryData) {
+            itemView.vSelected.isSelected = data.isSelected
+            itemView.tvNumber.isSelected = data.isSelected
+            if (data.isSelected) {
+                itemView.tvNumber.text = getIndexSelected(data).toString()
+            } else {
+                itemView.tvNumber.text = null
+            }
+        }
+
+    }
+
+
+    inner class MediaGalleryData(val data: MediaGallery) {
+        var isSelected = false
     }
 }
--- picker/src/main/java/com/dark/picker/activity/ImagePickerActivity.kt
+++ picker/src/main/java/com/dark/picker/activity/ImagePickerActivity.kt
@@ -1,19 +1,32 @@
 package com.dark.picker.activity
 
+import android.Manifest
 import android.app.Activity
+import android.content.ClipData
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.GridLayoutManager
 import com.dark.picker.CustomCallable
 import com.dark.picker.R
 import com.dark.picker.loadmore.LoadMoreRecyclerView
-import com.dark.picker.model.AlbumGallery
 import com.dark.picker.model.MediaGallery
 import com.dark.picker.repository.GalleryRepo
 import com.dark.picker.utils.safeLog
+import com.dark.picker.utils.setSafeOnClickListener
 import kotlinx.android.synthetic.main.activity_image_picker.*
 
+
 class ImagePickerActivity : Activity() {
+
+    companion object {
+        private const val CAMERA_REQUEST = 1888
+        private const val MY_CAMERA_PERMISSION_CODE = 100    }
 
     private val adapter: ImageAdapter by lazy {
         ImageAdapter()
@@ -31,7 +44,38 @@ class ImagePickerActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image_picker)
         rclvImagePickerImage.adapter = adapter
-        rclvImagePickerImage.setLoadDataListener(object : LoadMoreRecyclerView.IOnLoadMoreRecyclerViewListener {
+        initAction()
+        rclvImagePickerImage.layoutManager = GridLayoutManager(this, 3)
+        loadData()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == MY_CAMERA_PERMISSION_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show()
+                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(cameraIntent, CAMERA_REQUEST)
+            } else {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            setResult(RESULT_OK, data)
+            finish()
+        }
+    }
+
+    private fun initAction() {
+        rclvImagePickerImage.setLoadDataListener(object :
+            LoadMoreRecyclerView.IOnLoadMoreRecyclerViewListener {
             override fun onLoadData() {
                 loadData()
             }
@@ -40,8 +84,43 @@ class ImagePickerActivity : Activity() {
 
             }
         })
-        rclvImagePickerImage.layoutManager = GridLayoutManager(this, 3)
-        loadData()
+
+        adapter.onSelectItem = {
+            Log.e("NONAME", "onSelectItem $it")
+            ivImagePickerTakePhoto.isVisible = !it
+            tvImagePickerDone.isVisible = it
+        }
+        tvImagePickerDone.setSafeOnClickListener {
+            val intent = Intent()
+            var clip: ClipData? = null
+            val listUri = adapter.getListUriImageSelected()
+            for (uri in listUri) {
+                if (clip == null) {
+                    clip = ClipData(null, arrayOf(), ClipData.Item(uri))
+                } else {
+                    clip.addItem(ClipData.Item(uri))
+                }
+            }
+            intent.clipData = clip
+            setResult(RESULT_OK, intent)
+            finish()
+        }
+        ivImagePickerTakePhoto.setSafeOnClickListener {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.CAMERA
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.CAMERA),
+                    MY_CAMERA_PERMISSION_CODE
+                )
+            } else {
+                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(cameraIntent, CAMERA_REQUEST)
+            }
+        }
     }
 
     private fun loadData() {
@@ -49,7 +128,7 @@ class ImagePickerActivity : Activity() {
             return
         }
         isLoading = true
-        taskRunner.executeAsync<List<MediaGallery>>(object : CustomCallable<List<MediaGallery>?> {
+        taskRunner.executeAsync(object : CustomCallable<List<MediaGallery>?> {
             override fun setDataAfterLoading(result: List<MediaGallery>?) {
                 if (pageIndex == 0) {
                     adapter.reset(result)
@@ -59,6 +138,7 @@ class ImagePickerActivity : Activity() {
                 } else {
                     if (result.isNullOrEmpty()) {
                         stillMore = false
+                        adapter.showLoadMore(false)
                     } else {
                         stillMore = true
                         adapter.addMoreItem(result, stillMore)
@@ -74,7 +154,12 @@ class ImagePickerActivity : Activity() {
             override fun call(): List<MediaGallery>? {
                 var list: List<MediaGallery>? = null
                 try {
-                    list = GalleryRepo.getListGalleryPhoto(this@ImagePickerActivity, null, pageIndex, limit)
+                    list = GalleryRepo.getListGalleryPhoto(
+                        this@ImagePickerActivity,
+                        null,
+                        pageIndex,
+                        limit
+                    )
                     Log.e("NONAME", "$pageIndex --- ${list.size}")
                 } catch (e: Exception) {
                     e.safeLog()
